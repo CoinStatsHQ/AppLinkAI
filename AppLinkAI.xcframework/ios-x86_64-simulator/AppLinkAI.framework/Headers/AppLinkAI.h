0818246cc5b75865c75d1b085a8164eda437d653
Jenkins <jenkinscoinstats@gmail.com>
2023-08-03 12:15:11 +0400

    Updated framework version

---
+++ AppLinkAI.xcframework/ios-x86_64-simulator/AppLinkAI.framework/Headers/AppLinkAI.h
@@ -0,0 +1,18 @@
+//
+//  AppLinkAI.h
+//  AppLinkAI
+//
+//  Created by Artur Mkrtchyan on 8/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoinStatsLinks.
+FOUNDATION_EXPORT double AppLinkAIVersionNumber;
+
+//! Project version string for CoinStatsLinks.
+FOUNDATION_EXPORT const unsigned char AppLinkAIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppLinkAI/PublicHeader.h>
+
+
